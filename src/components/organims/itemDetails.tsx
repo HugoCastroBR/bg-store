cf6c4307517fc72eef5105c38e21f7212b7f2f99
HugoCastroBr <hugocastrohc@outlook.com>
2024-11-14 14:21:46 -0300

    fix: menor fixes

--- src/components/organims/itemDetails.tsx
+++ src/components/organims/itemDetails.tsx
@@ -42,7 +42,6 @@ const ItemDetails = (props:IItemDetailsProps) => {
           <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M11.7338 0.275313C11.3788 -0.0796359 10.8055 -0.0796359 10.4505 0.275313L6 4.71672L1.54949 0.266212C1.19454 -0.0887372 0.62116 -0.0887372 0.266212 0.266212C-0.0887372 0.62116 -0.0887372 1.19454 0.266212 1.54949L4.71672 6L0.266212 10.4505C-0.0887372 10.8055 -0.0887372 11.3788 0.266212 11.7338C0.62116 12.0887 1.19454 12.0887 1.54949 11.7338L6 7.28328L10.4505 11.7338C10.8055 12.0887 11.3788 12.0887 11.7338 11.7338C12.0887 11.3788 12.0887 10.8055 11.7338 10.4505L7.28328 6L11.7338 1.54949C12.0796 1.20364 12.0796 0.62116 11.7338 0.275313Z" fill="#4F372F" />
           </svg>
-
         </button>
 
         {/* Image and title section */}
@@ -63,15 +62,10 @@ const ItemDetails = (props:IItemDetailsProps) => {
             </Typography>
           </div>
         </div>
-
-        {/* Scrollable content */}
         <div className="relative flex-1 overflow-hidden">
-
           <ItemDetailsCategoryOption 
             {...props}
           />
-
-          {/* Fixed bottom section with blur effect */}
           <div className="absolute bottom-0 left-0 right-0 bg-white/30 backdrop-blur-[8px] h-[122px]">
             <div className="flex flex-col items-center h-full justify-evenly">
               <div className="flex items-center justify-between w-[143px] h-8 ">
@@ -107,9 +101,7 @@ const ItemDetails = (props:IItemDetailsProps) => {
                 </Typography>
               </FullButton>
             </div>
-
           </div>
-
         </div>
       </div>
     </div>
